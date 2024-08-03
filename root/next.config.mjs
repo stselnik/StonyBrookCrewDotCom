ec6f5122f2a06ef0cdd4f23f7e74ac129ca243ce
Patrick42702 <patrick42702@yahoo.com>
2024-08-03 07:47:25 -0400

    edited errors in history.tsx file

--- root/next.config.mjs
+++ root/next.config.mjs
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone',
+  output: 'export',
+  images: {
+    unoptimized: true,
+  }
 };
 
+const isProd = process.env.NODE_ENV === 'production';
+
+/*module.exports = {
+  basePath: isProd ? '/StonyBrookCrewDotCom' : '',
+  assetPrefix: isProd ? '/StonyBrookCrewDotCom' : '',
+};*/
 
 export default nextConfig;
